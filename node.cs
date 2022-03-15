592935b75ec97ddca3a19a200ea37fb5a91df7ef
chandana03 <chandanareshma01@gmail.com>
2022-03-15 22:57:57 +0530

    node

---
+++ node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Text;
+
+
+namespace linkedlist14
+{
+    public class node
+    {
+        public int data;
+        public node next;
+
+        public node(int data)
+        {
+            this.data = data;
+
+        }
+    }
+}
