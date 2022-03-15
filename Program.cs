8422146d0a8f19b9111ac1560e8e2834250f8a3e
chandana03 <chandanareshma01@gmail.com>
2022-03-15 22:49:55 +0530

    adding

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace linkedlist14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            LinkedList<int> linkedList = new LinkedList<int>();
+
+            linkedList.AddLast(56);
+            linkedList.AddLast(30);
+            linkedList.AddLast(70);
+           
+            foreach (int ele in linkedList)
+            {
+                Console.WriteLine(ele);
+            }
+        }
+    }
+}
