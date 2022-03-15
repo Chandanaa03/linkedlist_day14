eb5d5d3e5364b61498208418a7f9fe596fffdbb1
chandana03 <chandanareshma01@gmail.com>
2022-03-15 22:58:17 +0530

    list

---
+++ linkedlist.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linkedlist14
+{
+    public class linkedlist
+    {
+        public node head;
+
+        public void Add(int data)
+        {
+            node node = new node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+
+            Console.WriteLine("{0} inserted into Linked list", node.data);
+        }
+
+        public void Display()
+        {
+            node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked List is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
